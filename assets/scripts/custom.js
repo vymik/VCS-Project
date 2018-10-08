2a35bcbe1df4ef3b84969d2cfa8d781a3398b3a9
vymik <43873650+vymik@users.noreply.github.com>
2018-10-08 16:35:03 +0300

    add owl carousel

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,1 +1,26 @@
-console. log("Pavyko");
+$(document).ready(function(){
+	console.log("pavyko");
+
+$(".owl-carousel").owlCarousel();
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+
+
+});
